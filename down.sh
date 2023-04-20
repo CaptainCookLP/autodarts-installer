19f45e8f5bba1ef4c3a673b011a242c165a9c8ac
FKmedify | Fabi <67053133+CaptainCookLP@users.noreply.github.com>
2023-04-20 11:50:44 +0200

    Update down.sh

--- down.sh
+++ down.sh
@@ -8,6 +8,7 @@ wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/au
 wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/kiosk.sh
 wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/install.sh
 wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/main.sh
+wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/disclaimer.txt
 
 sudo chmod +x kiosk.sh
 sudo chmod +x install.sh
