edc9ab9039a3f8e7b34d18c088028fa08f8e2042
FKmedify | Fabi <67053133+CaptainCookLP@users.noreply.github.com>
2023-04-20 11:47:10 +0200

    Update down.sh

--- down.sh
+++ down.sh
@@ -4,10 +4,10 @@ echo Starting download.....
 mkdir ~/autodarts-installer
 cd ~/autodarts-installer
 
-https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/autostartkiosk.desktop
-https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/kiosk.sh
-https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/install.sh
-https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/main.sh
+wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/autostartkiosk.desktop
+wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/kiosk.sh
+wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/install.sh
+wget https://raw.githubusercontent.com/CaptainCookLP/autodarts-installer/main/main.sh
 
 sudo chmod +x kiosk.sh
 sudo chmod +x install.sh
